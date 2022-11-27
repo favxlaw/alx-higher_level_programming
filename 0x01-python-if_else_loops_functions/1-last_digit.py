a5590d0a039fc09340e412148c87345a71e76c1b
favxlaw <nmesomafavour2004@gmail.com>
2022-11-27 03:33:38 -0800

    tasks

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_number = abs(number) % 10
+digit = abs(number) % 10
 if number < 0:
-    last_number = -last_number
-if last_number > 5:
-    result = "greater than 5"
-elif last_number == 0:
-    result = "0"
+    digit = -digit
+print("Last digit of {} is {} and is ".format(number, digit), end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
 else:
-    result = "less than 6 and not 0"
-print(f"Last digit of {number:d}", f"is {last_number:d} and is", result
+    print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord("a"), ord("z") + 1):
-    if chr(i) == "q" or chr(i) == "e":
-        continue
-    else:
-        print("{:s}".format(chr(i)), end="")
+for a in range(ord('a'), ord('z') + 1):
+    if chr(a) != 'e' and chr(a) != 'q':
+        print("{:c}".format(a), end='')
